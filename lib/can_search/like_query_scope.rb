56d0fd23422fe72671bcfe932ff46789dc4bfabb
Matt <towski@gmail.com>
2008-07-30 15:26:35 -0700

    Adding a LikeQuery search scope to do LIKE database queries

---
+++ lib/can_search/like_query_scope.rb
@@ -0,0 +1,19 @@
+module CanSearch
+  # Generates a named scope for searching with a "LIKE ?" query. A format option can be specified 
+  # to change the string used for matching. The default matching string is "%?%".
+  class LikeQueryScope < BaseScope
+    def initialize(model, name, options = {})
+      super
+      @named_scope = options[:named_scope] || "like_#{name}".to_sym
+      @format      = options[:format]      || "%%%s%%"
+      @model.named_scope @named_scope, lambda { |q| {:conditions => ["#{@name} LIKE ?", @format % q]} }
+    end
+
+    def scope_for(finder, options = {})
+      query = options.delete(@name)
+      query.blank? ? finder : finder.send(@named_scope, query)
+    end
+  end
+  
+  SearchScopes.scope_types[:like] = LikeQueryScope
+end
